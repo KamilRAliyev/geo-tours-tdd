3dcba86119b50a2401e712fba5bbc5dd1ec9f0dc
Kamil Aliyev <kamilraliyev@gmail.com>
2021-10-24 17:54:14 +0300

    [+ Red] test for Model Creation

--- source/tours/tests/test_models.py
+++ source/tours/tests/test_models.py
@@ -1,2 +1,8 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
+from tours import models
 
+class TestTour(TestCase):
+    def test_model_creation(self):
+        obj = mixer.blend(models.Tour)
+        self.assertEqual(obj.pk, 1, "Tour is not creating") 
